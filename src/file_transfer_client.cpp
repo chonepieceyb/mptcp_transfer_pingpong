84bd82bbff372f531d9a6d989d6f1c8c86413840
chonepieceyb <1759315491@qq.com>
2021-12-10 14:28:13 +0800

    add send time

--- src/file_transfer_client.cpp
+++ src/file_transfer_client.cpp
@@ -3,9 +3,13 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iostream> 
+#include <chrono>
 #include "net_utils.h"
 
 namespace net {
+
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
 
 FileTransferClient::FileTransferClient(const std::string& dest_ip, const std:: string& dest_port, size_t send_buffer_size, bool enable_mptcp)
     :_send_buffer_size(send_buffer_size), 
@@ -23,13 +27,20 @@ void FileTransferClient::start_transfer(uint64_t kbytes) {
 
     sock.my_connect(_addr);
     int batch = ((kbytes << 10) / _send_buffer_size) + 1;
+    uint64_t ksend_data = (batch * _send_buffer_size) >> 10;
 
-    std::cout << "start send: " << ((batch * _send_buffer_size) >> 10) << " KB data\n";
+    std::cout << "start send: " << ksend_data << " KB data\n";
 
     std::string data(_send_buffer_size, 'a');
+
+    auto begin = high_resolution_clock::now();
     for (int i = 0; i < batch; i++) {
          sock.my_send(data);
     }
+    auto end = high_resolution_clock::now();
+    double time_interval = std::chrono::duration_cast<milliseconds>(end - begin).count() + 1e-5;
+
+    std::cout << "send time cost(ms): " << time_interval << " rate(kbyte/s): " << static_cast<double>(1000*ksend_data/time_interval) << std::endl; 
 }
 
 }
